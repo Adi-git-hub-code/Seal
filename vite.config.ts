fd1ea06ed809092c95f0315d006d5fe53ad2be5c
Adi-git-hub-code <adityasv22@gmail.com>
2026-05-08 08:29:23 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/seal/",
+  base: "/Seal/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
